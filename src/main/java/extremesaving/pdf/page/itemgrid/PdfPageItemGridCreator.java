e7d71ced38d2f5a0caadd8a30a8de91dcfe2d7eb
krisgregoire <kris.gregoire@osudio.com>
2019-10-21 17:42:03 +0200

    refactor

--- src/main/java/extremesaving/pdf/page/itemgrid/PdfPageItemGridCreator.java
+++ src/main/java/extremesaving/pdf/page/itemgrid/PdfPageItemGridCreator.java
@@ -1,19 +1,16 @@
 package extremesaving.pdf.page.itemgrid;
 
 import com.itextpdf.layout.Document;
-import com.itextpdf.layout.borders.Border;
-import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Table;
 import com.itextpdf.layout.property.TextAlignment;
-import com.itextpdf.layout.property.UnitValue;
 import extremesaving.calculation.dto.ResultDto;
 import extremesaving.calculation.facade.CalculationFacade;
 import extremesaving.data.facade.DataFacade;
 import extremesaving.pdf.page.PdfPageCreator;
+import extremesaving.pdf.page.itemgrid.section.ExpensesTablePdfSectionCreator;
+import extremesaving.pdf.page.itemgrid.section.ProfitsTablePdfSectionCreator;
 import extremesaving.pdf.util.PdfUtils;
 import extremesaving.util.DateUtils;
-import extremesaving.util.NumberUtils;
-import org.apache.commons.lang3.StringUtils;
 
 import java.util.Date;
 import java.util.List;
@@ -30,91 +27,37 @@ public class PdfPageItemGridCreator implements PdfPageCreator {
     @Override
     public void generate(Document document) {
         document.add(PdfUtils.getTitleParagraph("Most profitable items", TextAlignment.LEFT));
-        document.add(getProfitsTable());
+        document.add(buildProfitsTable());
         document.add(PdfUtils.getTitleParagraph("Most expensive items", TextAlignment.LEFT));
-        document.add(getExpensesTable());
+        document.add(buildExpensesTable());
     }
 
-    protected Table getProfitsTable() {
+    protected Table buildProfitsTable() {
         List<ResultDto> overallResults = calculationFacade.getMostProfitableItems(dataFacade.findAll());
         List<ResultDto> yearResults = calculationFacade.getMostProfitableItems(dataFacade.findAll().stream().filter(dataDto -> DateUtils.equalYears(new Date(), dataDto.getDate())).collect(Collectors.toList()));
         List<ResultDto> monthResults = calculationFacade.getMostProfitableItems(dataFacade.findAll().stream().filter(dataDto -> DateUtils.equalYearAndMonths(new Date(), dataDto.getDate())).collect(Collectors.toList()));
-
-        Table table = new Table(3);
-        table.setWidth(UnitValue.createPercentValue(100));
-        table.addCell(getItemCell("Overall", overallResults));
-        table.addCell(getItemCell("This year", yearResults));
-        table.addCell(getItemCell("This month", monthResults));
-        return table;
+        return new ProfitsTablePdfSectionCreator()
+                .withOverallResults(overallResults)
+                .withYearResults(yearResults)
+                .withMonthResults(monthResults)
+                .withDisplayMaxItems(DISPLAY_MAX_ITEMS)
+                .withDisplayMaxTextCharacters(TEXT_MAX_CHARACTERS)
+                .build()
+                .getTable();
     }
 
-    protected Table getExpensesTable() {
+    protected Table buildExpensesTable() {
         List<ResultDto> overallResults = calculationFacade.getMostExpensiveItems(dataFacade.findAll());
         List<ResultDto> yearResults = calculationFacade.getMostExpensiveItems(dataFacade.findAll().stream().filter(dataDto -> DateUtils.equalYears(new Date(), dataDto.getDate())).collect(Collectors.toList()));
         List<ResultDto> monthResults = calculationFacade.getMostExpensiveItems(dataFacade.findAll().stream().filter(dataDto -> DateUtils.equalYearAndMonths(new Date(), dataDto.getDate())).collect(Collectors.toList()));
-
-        Table table = new Table(3);
-        table.setWidth(UnitValue.createPercentValue(100));
-        table.addCell(getItemCell("Overall", overallResults));
-        table.addCell(getItemCell("This year", yearResults));
-        table.addCell(getItemCell("This month", monthResults));
-        return table;
-    }
-
-    protected Cell getItemCell(String title, List<ResultDto> results) {
-        Table alignmentTable = new Table(2);
-        alignmentTable.setPaddingLeft(0);
-        alignmentTable.setMarginLeft(0);
-        alignmentTable.setPaddingRight(0);
-        alignmentTable.setMarginRight(0);
-        alignmentTable.addCell(getLeftCell(results));
-        alignmentTable.addCell(getRightCell(results));
-
-        Cell cell = new Cell();
-        cell.add(PdfUtils.getItemParagraph(title, true, TextAlignment.CENTER));
-        cell.add(alignmentTable);
-        return cell;
-    }
-
-    protected Cell getLeftCell(List<ResultDto> results) {
-        Cell alignmentTableLeft = new Cell();
-        alignmentTableLeft.setBorder(Border.NO_BORDER);
-        alignmentTableLeft.setWidth(400);
-        alignmentTableLeft.setPaddingLeft(0);
-        alignmentTableLeft.setMarginLeft(0);
-        alignmentTableLeft.setPaddingRight(0);
-        alignmentTableLeft.setMarginRight(0);
-
-        int counter = 0;
-        for (ResultDto resultDto : results) {
-            counter++;
-            if (counter >= DISPLAY_MAX_ITEMS) {
-                break;
-            }
-            alignmentTableLeft.add(PdfUtils.getItemParagraph(StringUtils.abbreviate(resultDto.getData().iterator().next().getDescription(), TEXT_MAX_CHARACTERS)));
-        }
-        return alignmentTableLeft;
-    }
-
-    protected Cell getRightCell(List<ResultDto> results) {
-        Cell alignmentTableRight = new Cell();
-        alignmentTableRight.setBorder(Border.NO_BORDER);
-        alignmentTableRight.setTextAlignment(TextAlignment.RIGHT);
-        alignmentTableRight.setWidth(130);
-        alignmentTableRight.setPaddingLeft(0);
-        alignmentTableRight.setMarginLeft(0);
-        alignmentTableRight.setPaddingRight(0);
-        alignmentTableRight.setMarginRight(0);
-
-        int counter = 0;
-        for (ResultDto resultDto : results) {
-            counter++;
-            if (counter >= DISPLAY_MAX_ITEMS) {
-                break;
-            }
-            alignmentTableRight.add(PdfUtils.getItemParagraph(NumberUtils.formatNumber(resultDto.getResult())));
-        }
-        return alignmentTableRight;
+        return new ExpensesTablePdfSectionCreator()
+                .withOverallResults(overallResults)
+                .withYearResults(yearResults)
+                .withMonthResults(monthResults)
+                .withDisplayMaxItems(DISPLAY_MAX_ITEMS)
+                .withDisplayMaxTextCharacters(TEXT_MAX_CHARACTERS)
+                .build()
+                .getTable();
     }
 
     public void setDataFacade(DataFacade dataFacade) {
---
+++ src/main/java/extremesaving/pdf/page/itemgrid/section/ProfitsTablePdfSectionCreator.java
@@ -0,0 +1,117 @@
+package extremesaving.pdf.page.itemgrid.section;
+
+import com.itextpdf.layout.borders.Border;
+import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.property.TextAlignment;
+import com.itextpdf.layout.property.UnitValue;
+import extremesaving.calculation.dto.ResultDto;
+import extremesaving.pdf.util.PdfUtils;
+import extremesaving.util.NumberUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+
+public class ProfitsTablePdfSectionCreator {
+
+    private List<ResultDto> overallResults;
+    private List<ResultDto> yearResults;
+    private List<ResultDto> monthResults;
+    private int displayMaxItems;
+    private int displayMaxTextCharacters;
+    private Table table;
+
+    public ProfitsTablePdfSectionCreator withOverallResults(List<ResultDto> overallResults) {
+        this.overallResults = overallResults;
+        return this;
+    }
+
+    public ProfitsTablePdfSectionCreator withYearResults(List<ResultDto> yearResults) {
+        this.yearResults = yearResults;
+        return this;
+    }
+
+    public ProfitsTablePdfSectionCreator withMonthResults(List<ResultDto> monthResults) {
+        this.monthResults = monthResults;
+        return this;
+    }
+
+    public ProfitsTablePdfSectionCreator withDisplayMaxItems(int displayMaxItems) {
+        this.displayMaxItems = displayMaxItems;
+        return this;
+    }
+
+    public ProfitsTablePdfSectionCreator withDisplayMaxTextCharacters(int displayMaxTextCharacters) {
+        this.displayMaxTextCharacters = displayMaxTextCharacters;
+        return this;
+    }
+
+    public ProfitsTablePdfSectionCreator build() {
+        table = new Table(3);
+        table.setWidth(UnitValue.createPercentValue(100));
+        table.addCell(getItemCell("Overall", overallResults));
+        table.addCell(getItemCell("This year", yearResults));
+        table.addCell(getItemCell("This month", monthResults));
+        return this;
+    }
+
+    protected Cell getItemCell(String title, List<ResultDto> results) {
+        Table alignmentTable = new Table(2);
+        alignmentTable.setPaddingLeft(0);
+        alignmentTable.setMarginLeft(0);
+        alignmentTable.setPaddingRight(0);
+        alignmentTable.setMarginRight(0);
+        alignmentTable.addCell(getLeftCell(results));
+        alignmentTable.addCell(getRightCell(results));
+
+        Cell cell = new Cell();
+        cell.add(PdfUtils.getItemParagraph(title, true, TextAlignment.CENTER));
+        cell.add(alignmentTable);
+        return cell;
+    }
+
+    protected Cell getLeftCell(List<ResultDto> results) {
+        Cell alignmentTableLeft = new Cell();
+        alignmentTableLeft.setBorder(Border.NO_BORDER);
+        alignmentTableLeft.setWidth(400);
+        alignmentTableLeft.setPaddingLeft(0);
+        alignmentTableLeft.setMarginLeft(0);
+        alignmentTableLeft.setPaddingRight(0);
+        alignmentTableLeft.setMarginRight(0);
+
+        int counter = 0;
+        for (ResultDto resultDto : results) {
+            counter++;
+            if (counter >= displayMaxItems) {
+                break;
+            }
+            alignmentTableLeft.add(PdfUtils.getItemParagraph(StringUtils.abbreviate(resultDto.getData().iterator().next().getDescription(), displayMaxTextCharacters)));
+        }
+        return alignmentTableLeft;
+    }
+
+    protected Cell getRightCell(List<ResultDto> results) {
+        Cell alignmentTableRight = new Cell();
+        alignmentTableRight.setBorder(Border.NO_BORDER);
+        alignmentTableRight.setTextAlignment(TextAlignment.RIGHT);
+        alignmentTableRight.setWidth(130);
+        alignmentTableRight.setPaddingLeft(0);
+        alignmentTableRight.setMarginLeft(0);
+        alignmentTableRight.setPaddingRight(0);
+        alignmentTableRight.setMarginRight(0);
+
+        int counter = 0;
+        for (ResultDto resultDto : results) {
+            counter++;
+            if (counter >= displayMaxItems) {
+                break;
+            }
+            alignmentTableRight.add(PdfUtils.getItemParagraph(NumberUtils.formatNumber(resultDto.getResult())));
+        }
+        return alignmentTableRight;
+    }
+
+    public Table getTable() {
+        return table;
+    }
+}
