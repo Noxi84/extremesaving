36b93c3e401e8a47c926ff168af1b7fd9e6e8433
krisgregoire <kris.gregoire@osudio.com>
2020-07-05 19:09:44 +0200

    DateUtils.java

--- common/src/main/java/extremesaving/common/util/DateUtils.java
+++ common/src/main/java/extremesaving/common/util/DateUtils.java
@@ -15,6 +15,12 @@ public final class DateUtils {
     private DateUtils() {
     }
 
+    /**
+     * Get a sorted list of dates for the last 12 months before the given date.
+     *
+     * @param date Date to determine last 12 months.
+     * @return sorted list for last 12 months before the date.
+     */
     public static List<Date> getLastMonths(Date date) {
         Calendar lastDate = Calendar.getInstance();
         lastDate.setTime(date);
@@ -59,8 +65,53 @@ public final class DateUtils {
         return lastMonths;
     }
 
-    public static boolean isEqualYearAndMonth(List<Date> lastMonths, Date date) {
-        for (Date month : lastMonths) {
+    /**
+     * Get a Date object for the given day, month and year. Time will be set to 0.
+     *
+     * @param calendarDay Day of the month (1-31)
+     * @param calendarMonth Calendar month number. (0 = January, 11 = December)
+     * @param calendarYear Year
+     * @return Date object
+     */
+    public static Date getDate(int calendarDay, int calendarMonth, int calendarYear) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(calendarYear, calendarMonth, calendarDay, 0, 0, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        return cal.getTime();
+    }
+
+    /**
+     * Get the number of days between two given dates.
+     *
+     * @param d1 Date
+     * @param d2 Date
+     * @return number of days between two given dates
+     */
+    public static long getDaysBetween(Date d1, Date d2) {
+        return TimeUnit.DAYS.convert(d1.getTime() - d2.getTime(), TimeUnit.MILLISECONDS);
+    }
+
+    /**
+     * Check if two dates have the same day, month and year. Time will not be checked.
+     *
+     * @param d1 Date
+     * @param d2 Date
+     * @return true or false depending the two dates have the same day, month and year.
+     */
+    public static boolean isEqualDates(Date d1, Date d2) {
+        SimpleDateFormat sf = new SimpleDateFormat("DD/MM/YYYY");
+        return sf.format(d1).equals(sf.format(d2));
+    }
+
+    /**
+     * Check if all dates have the same month and year.
+     *
+     * @param months List of months to check
+     * @param date Date containing month and year to be matched.
+     * @return true or false depending if the months contain the same month and year.
+     */
+    public static boolean isEqualYearAndMonth(List<Date> months, Date date) {
+        for (Date month : months) {
             if (DateUtils.isEqualYearAndMonth(date, month)) {
                 return true;
             }
@@ -68,21 +119,13 @@ public final class DateUtils {
         return false;
     }
 
-    public static Date getDate(int calendarDay, int calendarMonth, int calendarYear) {
-        Calendar cal = Calendar.getInstance();
-        cal.set(calendarYear, calendarMonth, calendarDay, 0, 0, 0);
-        return cal.getTime();
-    }
-
-    public static long getDaysBetween(Date d1, Date d2) {
-        return TimeUnit.DAYS.convert(d1.getTime() - d2.getTime(), TimeUnit.MILLISECONDS);
-    }
-
-    public static boolean isEqualDates(Date d1, Date d2) {
-        SimpleDateFormat sf = new SimpleDateFormat("DD/MM/YYYY");
-        return sf.format(d1).equals(sf.format(d2));
-    }
-
+    /**
+     * Check if all dates have the same month.
+     *
+     * @param d1 List of months to check
+     * @param d2 Date containing month to be matched.
+     * @return true or false depending if the dates have the same month.
+     */
     public static boolean isEqualMonth(Date d1, Date d2) {
         Calendar cal = Calendar.getInstance();
         cal.setTime(d1);
@@ -91,10 +134,24 @@ public final class DateUtils {
         return cal.get(Calendar.MONTH) == cal2.get(Calendar.MONTH);
     }
 
+    /**
+     * Check if the dates have the same month and year.
+     *
+     * @param d1 date with month to check
+     * @param d2 Date containing month to be matched.
+     * @return true or false depending if the dates have the same month and year.
+     */
     public static boolean isEqualYearAndMonth(Date d1, Date d2) {
         return isEqualMonth(d1, d2) && isEqualYear(d1, d2);
     }
 
+    /**
+     * Check if all dates have the same year.
+     *
+     * @param d1 Date containing year to be checked.
+     * @param d2 Date containing month to be matched.
+     * @return true or false depending if the dates have the same year.
+     */
     public static boolean isEqualYear(Date d1, Date d2) {
         Calendar cal = Calendar.getInstance();
         cal.setTime(d1);
